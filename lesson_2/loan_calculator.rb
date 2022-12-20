5591ce48738f98c49123f09024dcdb006bd3fab9
Justin Berringer <justin.berringer@gmail.com>
2022-12-20 01:56:39 +0000

    making rubocop happier with loan_calculator.rb

--- lesson_2/loan_calculator.rb
+++ lesson_2/loan_calculator.rb
@@ -1,4 +1,4 @@
-#Method definitions
+# Method definitions
 def integer?(input)
   /^-?\d+$/.match(input)
 end
@@ -23,7 +23,7 @@ def prompt(message)
   puts ">> #{message}"
 end
 
-#Begin program
+# Begin program
 prompt("Let's check out your loan!")
 
 loop do # Main loop
@@ -31,7 +31,7 @@ loop do # Main loop
   loop do
     prompt("What is the total loan amount?")
     loan_amount = gets.chomp
-    
+
     if valid_number?(loan_amount)
       loan_amount = loan_amount.to_f
       break
@@ -39,13 +39,13 @@ loop do # Main loop
       prompt("Loan amount must be positive number.")
     end
   end
-  
+
   apr = nil
   monthly_interest = nil
   loop do
     prompt("What is the Annual Percentage Rate (APR)?")
     apr = gets.chomp
-    
+
     if valid_number?(apr)
       monthly_interest = ((apr.to_f / 100) / 12)
       break
@@ -53,13 +53,13 @@ loop do # Main loop
       prompt("Must enter valid APR.")
     end
   end
-  
+
   duration_in_years = nil
   duration_in_months = nil
   loop do
     prompt("What is the duration of the loan (in years)?")
     duration_in_years = gets.chomp
-    
+
     if valid_number?(duration_in_years)
       duration_in_months = years_to_months(duration_in_years)
       break
@@ -67,14 +67,13 @@ loop do # Main loop
       prompt("Must enter valid duration in years")
     end
   end
-  
+
   prompt("Your monthly payment is $#{monthly_payment(loan_amount, monthly_interest, duration_in_months).round(2)}")
   prompt("Your monthly interest rate is #{(monthly_interest * 100).round(2)}%")
   prompt("The duration of your loan is #{duration_in_months.to_i} months.")
-  
+
   prompt("Would you like to check another loan? (Y/N)")
   again = gets.chomp.downcase
-  
+
   break prompt("Thanks for using our loan calculator!") if again != "y"
-  
 end
