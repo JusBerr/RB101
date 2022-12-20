62ebf9b1b6839e7e26015ddd9e8abd6ca86e5e71
Justin Berringer <justin.berringer@gmail.com>
2022-12-20 01:39:45 +0000

    refactored loan_calculator.rb

--- lesson_2/loan_calculator.rb
+++ lesson_2/loan_calculator.rb
@@ -1,4 +1,4 @@
-
+#Method definitions
 def integer?(input)
   /^-?\d+$/.match(input)
 end
@@ -8,7 +8,7 @@ def float?(input)
 end
 
 def valid_number?(num)
-  integer?(num) || float?(num)
+  (integer?(num) || float?(num)) && num.to_f > 0
 end
 
 def years_to_months(years)
@@ -19,49 +19,62 @@ def monthly_payment(loan_amount, apr, duration_in_months)
   loan_amount * (apr / (1 - (1 + apr)**(-duration_in_months)))
 end
 
-puts "Let's check out your loan!"
-
-loan_amount = nil
-loop do
-  puts "What is the total loan amount?"
-  loan_amount = gets.chomp
-  
-  if valid_number?(loan_amount)
-    loan_amount = loan_amount.to_f
-    break
-  else 
-    puts "Must enter a valid load amount."
-  end
+def prompt(message)
+  puts ">> #{message}"
 end
 
-apr = nil
-monthly_interest = nil
-loop do
-  puts "What is the Annual Percentage Rate (APR)?"
-  apr = gets.chomp
-  
-  if valid_number?(apr)
-    monthly_interest = ((apr.to_f / 100) / 12)
-    break
-  else
-    puts "Must enter valid APR."
-  end
-end
+#Begin program
+prompt("Let's check out your loan!")
 
-duration_in_years = nil
-duration_in_months = nil
-loop do
-  puts "What is the duration of the loan? (in years)"
-  duration_in_years = gets.chomp
-  
-  if valid_number?(duration_in_years)
-    duration_in_months = years_to_months(duration_in_years)
-    break
-  else
-    puts "Must enter valid duration in years"
+loop do # Main loop
+  loan_amount = nil
+  loop do
+    prompt("What is the total loan amount?")
+    loan_amount = gets.chomp
+    
+    if valid_number?(loan_amount)
+      loan_amount = loan_amount.to_f
+      break
+    else
+      prompt("Loan amount must be positive number.")
+    end
   end
+  
+  apr = nil
+  monthly_interest = nil
+  loop do
+    prompt("What is the Annual Percentage Rate (APR)?")
+    apr = gets.chomp
+    
+    if valid_number?(apr)
+      monthly_interest = ((apr.to_f / 100) / 12)
+      break
+    else
+      prompt("Must enter valid APR.")
+    end
+  end
+  
+  duration_in_years = nil
+  duration_in_months = nil
+  loop do
+    prompt("What is the duration of the loan (in years)?")
+    duration_in_years = gets.chomp
+    
+    if valid_number?(duration_in_years)
+      duration_in_months = years_to_months(duration_in_years)
+      break
+    else
+      prompt("Must enter valid duration in years")
+    end
+  end
+  
+  prompt("Your monthly payment is $#{monthly_payment(loan_amount, monthly_interest, duration_in_months).round(2)}")
+  prompt("Your monthly interest rate is #{(monthly_interest * 100).round(2)}%")
+  prompt("The duration of your loan is #{duration_in_months.to_i} months.")
+  
+  prompt("Would you like to check another loan? (Y/N)")
+  again = gets.chomp.downcase
+  
+  break prompt("Thanks for using our loan calculator!") if again != "y"
+  
 end
-
-puts "Your monthly payment is $#{monthly_payment(loan_amount, monthly_interest, duration_in_months).round(2)}"
-puts "Your monthly interest rate is #{(monthly_interest * 100).round(2)}%"
-puts "The duration of your loan is #{duration_in_months.to_i} months."
