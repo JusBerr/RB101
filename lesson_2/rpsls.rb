575d3794f04d209720c8c4d6f4281fefc5bf08c6
Justin Berringer <justin.berringer@gmail.com>
2023-01-19 04:08:38 +0000

    Refactoring + added YAML messages

--- lesson_2/rpsls.rb
+++ lesson_2/rpsls.rb
@@ -13,7 +13,7 @@ WIN_CONDITIONS = {
   rock: ['lizard', 'scissors'],
   paper: ['rock', 'spock'],
   scissors: ['paper', 'lizard'],
-  spock: ['rock', 'paper'],
+  spock: ['rock', 'scissors'],
   lizard: ['paper', 'spock']
 }
 
@@ -36,7 +36,7 @@ def display_rules?
   answer = gets.chomp.downcase
   if answer.start_with?("y")
     prompt(MESSAGES["rules"])
-    sleep(3)
+    gets
   end
 end
 
@@ -101,6 +101,47 @@ def play_again?
   answer.start_with?('y')
 end
 
+# Thanks Brandi!
+
+def get_player_move(computer_score)
+  loop do
+    prompt(MESSAGES["player_move"])
+    add_suspense if computer_score == (WINNING_SCORE - 1)
+    player_move = gets.chomp.downcase
+    player_move = VALID_MOVES[player_move]
+    return player_move if valid_choice?(player_move)
+  end
+end
+
+def random_move
+  COMPUTER_MOVES.sample.to_sym
+end
+
+def display_selected_moves(player_move, computer_move)
+  prompt("\t#{@user_name}'s #{player_move} VS"\
+         " Computer's #{computer_move}...")
+end
+
+def begin_match_message
+  prompt("Alright, #{@user_name}... let the games begin!")
+end
+
+def update_player_score(player_move, computer_move, player_score)
+  if round_winner(player_move, computer_move) == :player_win
+    player_score + 1
+  else
+    player_score
+  end
+end
+
+def update_computer_score(player_move, computer_move, computer_score)
+  if round_winner(player_move, computer_move) == :computer_win
+    computer_score + 1
+  else
+    computer_score
+  end
+end
+
 # Begin program
 prompt(MESSAGES["welcome"])
 sleep(1)
@@ -110,7 +151,9 @@ sleep(1)
 system("clear")
 
 display_rules?
-prompt("Alright, #{@user_name}... let the games begin!")
+
+begin_match_message
+
 sleep(1)
 loop do
   player_score = 0
@@ -118,37 +161,32 @@ loop do
   display_current_score(player_score, computer_score)
 
   until player_score == WINNING_SCORE || computer_score == WINNING_SCORE
-    player_move = ""
-    computer_move = COMPUTER_MOVES.sample.to_sym
-    loop do
-      prompt(MESSAGES["player_move"])
 
-      add_suspense if computer_score == (WINNING_SCORE - 1)
+    computer_move = random_move
 
-      player_move = gets.chomp.downcase
-      player_move = VALID_MOVES[player_move]
-      break if valid_choice?(player_move)
-    end
+    # computer_score is passed in to determine if add_suspense
+    player_move = get_player_move(computer_score)
 
     sleep(0.3)
     system("clear")
 
-    prompt("\t#{@user_name}'s #{player_move} VS"\
-           " Computer's #{computer_move}...")
+    display_selected_moves(player_move, computer_move)
 
     display_round_result(player_move, computer_move)
 
-    case round_winner(player_move, computer_move)
-    when :player_win
-      player_score += 1
-    when :computer_win
-      computer_score += 1
-    end
+    player_score = update_player_score(player_move, computer_move,
+                                       player_score)
+    computer_score = update_computer_score(player_move, computer_move,
+                                           computer_score)
 
     display_current_score(player_score, computer_score)
   end
 
   display_grand_winner(player_score, computer_score)
+
   break unless play_again?
   system("clear")
 end
+
+prompt(MESSAGES["goodbye"])
+sleep(3)
